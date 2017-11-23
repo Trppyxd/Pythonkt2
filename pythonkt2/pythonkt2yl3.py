335d5e0ac4c77c9d7fbbc68cf002bf993f4202ec
Trppyxd <33310226+Trppyxd@users.noreply.github.com>
2017-11-23 09:32:54 +0200

    Parandus

--- pythonkt2/pythonkt2yl3.py
+++ pythonkt2/pythonkt2yl3.py
@@ -1,7 +1,7 @@
 a = [11, 15, 6, 13, 13, 25, 32, 11, 20, 5, 31, 16, 32, 29, 11, 13, 3, 29, 28, 24]
 b = [5, 19, 16, 4, 12, 7, 2, 28, 34, 29, 29, 36, 6, 8, 28, 18, 31, 7, 1, 7]
 #1
-print ("Same numbers", set(a)&set(b))
+print ("Sama numbrid", set(a)&set(b))
 #2
 maxA = (max(a))
 maxB = (max(b))
@@ -19,16 +19,17 @@ list2 = [minA, minB]
 minAll = (min(list2))
 print ("Min number",minAll)
 #4
-aAmount = (21)
+aAmount = (20)
 aSum = (sum(a))
-aKeskmine = (aSum / 21)
-print ("Keskmine on ", aKeskmine)
+aKeskmine = (aSum / 20)
+print ("A Keskmine on ", aKeskmine)
 
 bAmount = (20)
 bSum = (sum(b))
-bKeskmine = (bSum / 21)
-print ("Keskmine on ", bKeskmine)
+bKeskmine = (bSum / 20)
+print ("B Keskmine on ", bKeskmine)
 #5
-abAmount = (41)
+abAmount = (40)
 abSum = (aSum + bSum)
-abKeskmine = (
+abKeskmine = (abSum / 40)
+print ("AB Keskmine on ", abKeskmine)
